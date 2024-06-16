1066b2178c6520e161b67e90e672bddbeeea0638
flyq <flyq951@gmail.com>
2024-06-16 15:05:02 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ pub mod valida;
 
 fn main() {
     println!("Hello, world!");
-    let proof_bytes = fs::read("../llvm-valida-alpha-mac-build/output/fib.proof").unwrap();
-    let executable_file = fs::read("../llvm-valida-alpha-mac-build/output/fib.out").unwrap();
+    let proof_bytes = fs::read("./llvm-valida-alpha-mac-build/output/fib.proof").unwrap();
+    let executable_file = fs::read("./llvm-valida-alpha-mac-build/output/fib.out").unwrap();
 
     let res = valida::verify_valida_proof(proof_bytes, executable_file, None, None);
     println!("res: {:?}", res);
